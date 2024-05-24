605fd9896f9434b2f41cdb6a10c56095fd352098
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-05-24 19:56:51 +0530

    get information when same tab is updated

---
+++ client/public/onUpdateTab.js
@@ -0,0 +1,24 @@
+//on update tabs events
+chrome.tabs.onUpdated.addListener((p, t, tab2) => {
+    if (tab2.url != "chrome://newtab/" && preTab != tab2.url) {
+      if (preTab != "" && activeTabs[preTab]) {
+        stopTimer();
+        activeTabs[preTab].totalTime = `${hours}:${minutes}:${seconds}`;
+        console.log(preTab);
+        console.log(activeTabs[preTab].totalTime);
+        resetTimer();
+      }
+  
+      if (tab2.active && tab2.windowId !== chrome.windows.WINDOW_ID_NONE) {
+        startTimer();
+        if (!activeTabs[tab2.url]) {
+          activeTabs[tab2.url] = {
+            totalTime: `${hours}:${minutes}:${seconds}`,
+          };
+        }
+      }
+      console.log(activeTabs);
+      preTab = tab2.url;
+    }
+  });
+  
