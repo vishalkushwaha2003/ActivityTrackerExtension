29be5ebdb8912cdedd11c4938dae7d6936d08ec3
Ramandeep Singh <ramandeepsingh40270@gmail.com>
2024-06-01 14:10:34 +0530

    Added the UI for search button

--- client/src/components/BlockStats/BlockStats.jsx
+++ client/src/components/BlockStats/BlockStats.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable no-unused-vars */
 import { useEffect, useState } from "react";
 import BlockCard from "./BlockCard";
+import SearchIcon from "@mui/icons-material/Search";
+import { TextField } from "@mui/material";
 
 const BlockStats = () => {
   const [data, setData] = useState([
@@ -11,26 +13,71 @@ const BlockStats = () => {
       endTime: "12:00",
       days: [true, true, true, false, false, false, false],
     },
+    {
+      url: "www.google.com",
+      name: "Netflix",
+      startTime: "10:00",
+      endTime: "12:00",
+      days: [true, true, true, false, false, false, false],
+    },
+    {
+      url: "www.google.com",
+      name: "Facebook",
+      startTime: "10:00",
+      endTime: "12:00",
+      days: [true, true, true, false, false, false, false],
+    },
+    {
+      url: "www.google.com",
+      name: "Youtube",
+      startTime: "10:00",
+      endTime: "12:00",
+      days: [true, true, true, false, false, false, false],
+    },
   ]);
+
+  const [filteredData, setFilteredData] = useState([]);
 
   useEffect(() => {
     // call for getting the data
+    setFilteredData(data);
   }, []);
+
+  const searchHandler = (e) => {
+    console.log(e.target.value);
+  };
 
   return (
     <div className="flex flex-col justify-center items-center ">
-      <div className="text-[#c7c7c6] mt-2 text-base pb-2">
+      <div className="absolute right-[32px] top-10">
+        {
+          <input
+            className=" px-2 py-1 rounded-md text-[#9b9b9a] bg-[rgba(47,47,47,255)] w-[100px]  border-none focus:outline-none"
+            type="text"
+            onChange={searchHandler}
+            placeholder="Search"
+            id="urlInput"
+          />
+        }
+      </div>
+
+      <div className="text-[#c7c7c6] mt-2 text-base pb-2 ml-[-120px] ">
         <h1>All Blocked Sites</h1>
       </div>
 
       <div className="h-[310px] overflow-y-scroll">
-        <BlockCard
-          url={data[0].url}
-          name={data[0].name}
-          startTime={data[0].startTime}
-          endTime={data[0].endTime}
-          days={data[0].days}
-        />
+        {filteredData.map((item, i) => {
+          return (
+            <BlockCard
+              key={i}
+              url={item.url}
+              name={item.name}
+              startTime={item.startTime}
+              endTime={item.endTime}
+              days={item.days}
+            />
+          );
+        })}
       </div>
     </div>
   );
